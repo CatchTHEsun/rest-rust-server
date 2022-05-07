2f773c1e26e1683d532156973531b001f77e6185
Andrey <notmymaster@gmail.com>
2022-05-07 21:05:41 +0300

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![feature(decl_macro)]
+
+#[macro_use]
+extern create diesel;
+#[macro_use]
+extern create diesel_migrations;
+#[macro_use]
+extern create rocket;
+#[macro_use]
+extern create rocket_contrib;
+
+use create::database::TimesheetsDatabaseInitialized;
+use create::routes::TimesheetsRoutesInitialized;
+
+mod routes;
+pub mod handlers;
+pub mod database;
+mod config;
+mod schema;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::custom(config::from_env())
+        .manage_database()
+        .mount_timesheet_routes()
+        .launch();
 }
