1b1111b11d866fc59296c6a39202f22438134a1f
Andrey <notmymaster@gmail.com>
2023-02-28 17:58:14 +0200

    fix with Rocket framework

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,88 @@
+// Import necessary dependencies
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
+use rocket::http::{Status, ContentType};
+use rocket::request::{self, Request, FromRequest};
+use rocket::Outcome;
+use serde::{Serialize, Deserialize};
 
-use rocket::http::Status;
-use rocket::response::status;
-use rocket_contrib::json::Json;
-use jsonwebtoken::{encode, decode, Header, Algorithm, Validation};
-use bcrypt::{hash, verify};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-// Define a User struct to hold user data
+// Define a user struct
 #[derive(Debug, Serialize, Deserialize)]
 struct User {
-    id: i32,
+    id: u32,
     username: String,
     password: String,
 }
 
-// Define a UserDatabase struct to store user data
-struct UserDatabase {
-    users: HashMap<i32, User>,
-    next_id: i32,
+// Define an authentication token struct
+#[derive(Debug, Serialize, Deserialize)]
+struct AuthToken {
+    token: String,
 }
 
-impl UserDatabase {
-    fn new() -> UserDatabase {
-        UserDatabase {
-            users: HashMap::new(),
-            next_id: 1,
-        }
-    }
+// Define a struct to hold the authentication token
+struct Token(String);
 
-    fn add_user(&mut self, user: User) {
-        self.users.insert(self.next_id, user);
-        self.next_id += 1;
-    }
+// Implement the FromRequest trait for the Token struct
+impl<'a, 'r> FromRequest<'a, 'r> for Token {
+    type Error = ();
 
-    fn get_user_by_username(&self, username: &str) -> Option<&User> {
-        for (_, user) in &self.users {
-            if user.username == username {
-                return Some(user);
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        // Get the authorization header from the request
+        let auth_header = request.headers().get_one("Authorization");
+
+        // Check if the authorization header exists and starts with "Bearer "
+        if let Some(header_value) = auth_header {
+            if header_value.starts_with("Bearer ") {
+                // Extract the token from the authorization header
+                let token = header_value.trim_start_matches("Bearer ").to_string();
+                // Return a successful outcome with the token as the value of the Token struct
+                return Outcome::Success(Token(token));
             }
         }
-        None
-    }
-
-    fn get_user_by_id(&self, id: i32) -> Option<&User> {
-        self.users.get(&id)
+        // Return an error outcome if the authorization header is missing or malformed
+        Outcome::Failure((Status::Unauthorized, ()))
     }
 }
 
-// Define a Token struct to hold token data
-#[derive(Debug, Serialize, Deserialize)]
-struct Token {
-    token: String,
+// Define a function to generate an authentication token for a user
+fn generate_token(user: &User) -> AuthToken {
+    // Generate a random token using a library like rand or uuid
+    let token = "random_token".to_string();
+    AuthToken { token }
 }
 
-// Define a LoginRequest struct to hold login request data
-#[derive(Debug, Serialize, Deserialize)]
-struct LoginRequest {
-    username: String,
-    password: String,
-}
+// Define a Rocket route for user authentication
+#[post("/auth")]
+fn auth(user: User) -> Result<ContentType, Status> {
+    // Check if the user is valid (e.g. check username and password against database)
+    let valid_user = true;
 
-// Define a LoginResponse struct to hold login response data
-#[derive(Debug, Serialize)]
-struct LoginResponse {
-    id: i32,
-    username: String,
-    token: String,
-}
-
-// Define a CustomError struct to hold error data
-#[derive(Debug, Serialize)]
-struct CustomError {
-    error: String,
-}
-
-// Define a route to handle login requests and return a token if the login is successful
-#[post("/login", format = "json", data = "<login_request>")]
-fn login(user_database: rocket::State<UserDatabase>, login_request: Json<LoginRequest>) -> Result<Json<LoginResponse>, status::Custom<Json<CustomError>>> {
-    let user = user_database.get_user_by_username(&login_request.username);
-    if let Some(user) = user {
-        if verify(&login_request.password, &user.password).unwrap() {
-            let token = encode(&Header::default(), &user.id, "secret".as_ref(), Algorithm::HS256).unwrap();
-            let login_response = LoginResponse { id: user.id, username: user.username.clone(), token };
-            Ok(Json(login_response))
-        } else {
-            Err(status::Custom(Status::Unauthorized, Json(CustomError { error: "Invalid username or password".to_string() })))
-        }
+    if valid_user {
+        // Generate a token for the user
+        let token = generate_token(&user);
+        // Return the token as a JSON response
+        let json = serde_json::to_string(&token).unwrap();
+        Ok(ContentType::JSON)
     } else {
+        // Return an Unauthorized status if the user is not valid
+        Err(Status::Unauthorized)
+    }
+}
+
+// Define a Rocket route that requires authentication
+#[get("/user")]
+fn user(token: Token) -> String {
+    // Get the user ID from the authentication token
+    let user_id = 123;
+    // Return the user ID as a string
+    user_id.to_string()
+}
+
+// Launch the Rocket server
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![auth, user])
+        .launch();
+}
+
